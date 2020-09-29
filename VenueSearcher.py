d9e10b9cc15a362c1ca04645578b98ddd32ba711
lagvna <lagvna@gmail.com>
2020-09-29 16:49:49 +0200

    Dodalem wyszukiwanie po filtrach i kilka dodatkow

--- VenueSearcher.py
+++ VenueSearcher.py
@@ -13,12 +13,39 @@ class VenueSearcher:
 			venues = list(csv_data)
 		except:
 			print("Nie znaleziono pliku!\n")
-			
+
 		results = []
 		for data_line in venues:
 			if  title.lower() in data_line[1].lower():
 				result = Venue(data_line)
 				results.append(result)
 				#print(results)
+
+		return results
+
+	def load_everything(self):
+		try:
+			data = open(self.path, encoding="utf-8")
+			csv_data = csv.reader(data)
+			venues = list(csv_data)
+		except:
+			print("Nie znaleziono pliku!\n")
+
+		results = []
+		for data_line in venues:
+			result = Venue(data_line)
+			results.append(result)
+				#print(results)
+		return results
+
+	def search_by_params(self, params_dict):
+		results = self.load_everything()
+
+		if '-t' in params_dict.keys():
+			results = [x for x in results if params_dict['-t'].lower() in x.title.lower()]
+		if '-s' in params_dict.keys():
+			results = [x for x in results if params_dict['-s'] == x.score]
+		if '-c' in params_dict.keys():
+			results = [x for x in results if params_dict['-c'] in x.category]
 
 		return results
--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from VenueSearcher import *
+import csv
+from matplotlib import pyplot as plt 
+import numpy as np 
 
 def print_help():
 	print("POMOC\n")
+	print("Składnia polecenia filtrowania to:")
+	print("-t:nazwa, -s:punktacja, -c:kategoria\n")
 
 def search_venue():
 	try:
@@ -21,8 +26,51 @@ def search_venue():
 		print("Nie znaleziono podanego czasopisma\n")
 
 	
+def filter_venues():
+	try:
+		command = input("Wpisz polecenie filtrowania wyników: \n")
+		params = command.split(',')
+		params_dict = dict((x.strip(), y.strip()) 
+			for x, y in (element.split(':')
+				for element in command.split(', ')))
+		#print(params_dict)
+		searcher = VenueSearcher("resources/wykaz.csv")
+		results = searcher.search_by_params(params_dict)
+		
+		print(f"Znaleziono {len(results)} pozycje: \n")
+		filename = input("Podaj nazwę pliku, w którym chciałbyś zapisać wyniki: \n")
+		file_to_output = open(filename+'.csv','w',newline='')
+		csv_writer = csv.writer(file_to_output,delimiter=',')
+		
+		for item in results:
+			csv_writer.writerow([item.title,item.issn,item.eissn,item.score,item.category])
 
-print("Program wspomagający wyszukiwanie informacji w aktualnym wykazie czasopism MNiSW\n")
+			print(item.title)
+			print(f"ISSN: {item.issn}")
+			print(f"E-ISSN: {item.eissn}")
+			print(f"Punktacja: {item.score}")
+			print(f"Kategoria: {item.category}")
+			print()
+
+		file_to_output.close()
+	except:
+		print("Nieprawidłowe polecenie\n")
+
+
+
+def draw_chart():
+
+	# Creating dataset 
+	cars = ['AUDI', 'BMW', 'FORD', 
+        		'TESLA', 'JAGUAR', 'MERCEDES'] 
+	data = [23, 17, 35, 29, 12, 41] 
+
+	fig = plt.figure(figsize =(10, 7)) 
+	plt.pie(data, labels = cars) 
+  
+	plt.savefig("mygraph.png")
+
+print("Program wspomagający wyszukiwanie informacji w aktualnym wykazie czasopism punktowanych MNiSW\n")
 
 while True:
 	print("(1) Wyszukaj czasopismo po nazwie")
@@ -38,9 +86,9 @@ while True:
 		if choice == 4:
 			break
 		elif choice == 3:
-			print_help()
+			draw_chart()
 		elif choice == 2:
-			pass
+			filter_venues()
 		elif choice == 1:
 			search_venue()
 		else:
